c5c81425a8e2ada2da308d5743533e0092369914
Gabriel M. Severio <83481136+Mitzuki01@users.noreply.github.com>
2022-10-18 11:48:21 -0300

    Update MainActivity.java
    finalização do aplicativo para verificação de triângulos

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,18 +2,31 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+   // Context mensager = getApplicationContext();
+   // String mensM = "hello world";
+   // int duration = Toast.LENGTH_SHORT;
+   // Toast finish = Toast.makeText(mensager,mensM,duration);
+   // finish.show();
+
+
+
+
+
     EditText ladoA, ladoB, ladoC;
-    TextView result, result3;
+    TextView result, result2;
     Button verificar;
-    public float triangulo, triangulo2;
+    public float trianguloA, trianguloB, trianguloC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         ladoC = (EditText) findViewById(R.id.ladoc);
         verificar = (Button) findViewById(R.id.verificar);
         result = (TextView) findViewById(R.id.result);
-      //  result3 = (TextView) findViewById(R.id.result3);
+        result2 = (TextView) findViewById(R.id.result2);
 
         verificar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,47 +47,43 @@ public class MainActivity extends AppCompatActivity {
                 float ladoAn = Float.parseFloat((ladoA.getText().toString()));
                 float ladoBn = Float.parseFloat((ladoB.getText().toString()));
                 float ladoCn = Float.parseFloat((ladoC.getText().toString()));
+                trianguloA = ladoBn+ladoCn;
+                trianguloB = ladoAn+ladoCn;
+                trianguloC = ladoAn+ladoBn;
 
-                if (ladoAn > ladoBn && ladoAn > ladoCn){
-                    triangulo = ladoBn+ladoCn;
-                    if(triangulo > ladoAn){
-                        result.setText("isto é um triangulo");
 
-                       if (ladoAn == ladoBn && ladoAn == ladoCn && ladoBn == ladoCn){
-                            result3.setText("Equilatero");
-                        }
+                if ((ladoAn >= ladoBn && ladoAn >= ladoCn && trianguloA >= ladoAn ) || (ladoBn >= ladoAn && ladoBn >= ladoCn && trianguloB >= ladoBn) || (ladoCn >= ladoAn && ladoCn >= ladoBn && trianguloC >= ladoCn)) {
+                    result.setText("Isto é um triangulo");
+                    Toast.makeText(getApplicationContext(),"hello world", Toast.LENGTH_LONG).show();
+
+
+                    if ((ladoAn == ladoBn && ladoAn == ladoCn && ladoBn == ladoCn)) {
+                        result2.setText("isto é um equilatero");
+
+
+                    } else if(ladoAn != ladoBn && ladoAn != ladoCn && ladoBn != ladoCn) {
+                        result2.setText("isto é um escaleno");
+
+                    }else{
+                        result2.setText("isto é um isoceles");
+
 
                     }
-                    else{
-                        result.setText("isto não é um triangulo");
-                    }
+                }
+                else {
+                    result.setText("Isto não é um triangulo");
+                    result2.setText("");
+
+
                 }
 
 
-                if (ladoBn > ladoAn && ladoBn > ladoCn){
-                    triangulo = ladoAn+ladoCn;
-                    if(triangulo > ladoBn){
-                        result.setText("isto é um triangulo");
-                    }
-                    else{
-                        result.setText("isto não é um triangulo");
-                    }
 
                 }
 
 
-                if (ladoCn > ladoAn && ladoCn > ladoBn){
-                    triangulo = ladoBn+ladoAn;
-                    if(triangulo > ladoCn){
-                        result.setText("isto é um triangulo");
-                    }
-                    else{
-                        result.setText("isto não é um triangulo");
-                    }
 
-                }
 
-            }
         });
 
 
